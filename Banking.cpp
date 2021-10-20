32c5a8aee2c35064d55fcad23839b2322b2bec7a
coder-tanvir <tanvir_khan@sfu.ca>
2021-10-19 23:21:54 -0700

    added most of the features

--- Banking.cpp
+++ Banking.cpp
@@ -6,16 +6,39 @@ class Bank{
         char name[70];
         char address[70];
         float balance;
+        float withdraw;
         float deposit;
-        float withdrawal;
+        
+    
     public:
         
         char* getname();
         char* getaddress();
         float getbalance();
         void openaccount();
+        void depositmoney();
+        void withdrawwmoney();
+        void accountdetails();
 
 };
+char* Bank::getname( ){
+    return name;
+}
+
+float Bank::getbalance(){
+    return balance;
+}
+
+char* Bank::getaddress(){
+    return address;
+}
+
+void Bank::depositmoney(){
+    float amount;
+    cout<<"How Much You want to deposit in $"<<endl;
+    cin>>deposit;
+    balance=balance+deposit;
+}
 
 void Bank::openaccount(){
     cout<<"Please enter your Name"<<endl;
@@ -28,23 +51,25 @@ void Bank::openaccount(){
     cin>>balance;   
 }
 
-char* Bank::getname( ){
-    return name;
+void Bank::withdrawwmoney(){
+    float amount;
+    cout<<"How much you want to withdraw"<<endl;
+    cin>>withdraw;
+    balance=balance-withdraw;
 }
+void Bank::accountdetails(){
+    cout<<"Name: "<<this->name<<endl;
+    cout<<"Address: "<<this->address<<endl;
+    cout<<"Your current balance"<<this->balance<<endl;
+    cout<<"Your last withdraw"<<this->withdraw<<endl;
+    cout<<"Your last deposit"<<this->deposit<<endl;
 
-float Bank::getbalance(){
-    return balance;
-}
-
-char* Bank::getaddress(){
-    return address;
 }
 int main(){
     
     Bank obj1;
-    obj1.openaccount();
-    cout<< obj1.getname();
-    cout<<obj1.getbalance();
-    cout<<obj1.getaddress();
+    cout<<"-------------------Welcome To Banking App----------------------"<<endl;
+    cout<<"---------------------------------------------------------------"<<endl;
+    cout<<"Please Choose an option below"<<endl;
     
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -6,31 +6,21 @@ class Bank{
         char name[70];
         char address[70];
         float balance;
+        float withdraw;
         float deposit;
-        float withdrawal;
+        
+    
     public:
         
         char* getname();
         char* getaddress();
         float getbalance();
         void openaccount();
+        void depositmoney();
+        void withdrawwmoney();
+        void accountdetails();
 
 };
-
-void Bank::openaccount(){
-    cout<<"Please enter your Name"<<endl;
-    
-    cin.getline(name,100);
-    
-    cout<<"Please enter your Full Address"<<endl;
-    cin.ignore();
-    cin.getline(address,70);
-
-    cout<<"Please enter the amount($) you want to deposit"<<endl;
-    cin.ignore();
-    cin>>balance;   
-}
-
 char* Bank::getname( ){
     return name;
 }
@@ -42,6 +32,39 @@ float Bank::getbalance(){
 char* Bank::getaddress(){
     return address;
 }
+
+void Bank::depositmoney(){
+    float amount;
+    cout<<"How Much You want to deposit in $"<<endl;
+    cin>>deposit;
+    balance=balance+deposit;
+}
+
+void Bank::openaccount(){
+    cout<<"Please enter your Name"<<endl;
+    cin.getline(name,100);
+    
+    cout<<"Please enter your Full Address"<<endl;
+    cin.getline(address,70);
+
+    cout<<"Please enter the amount($) you want to deposit"<<endl;
+    cin>>balance;   
+}
+
+void Bank::withdrawwmoney(){
+    float amount;
+    cout<<"How much you want to withdraw"<<endl;
+    cin>>withdraw;
+    balance=balance-withdraw;
+}
+void Bank::accountdetails(){
+    cout<<"Name: "<<this->name<<endl;
+    cout<<"Address: "<<this->address<<endl;
+    cout<<"Your current balance"<<this->balance<<endl;
+    cout<<"Your last withdraw"<<this->withdraw<<endl;
+    cout<<"Your last deposit"<<this->deposit<<endl;
+
+}
 int main(){
     
     Bank obj1;
@@ -49,5 +72,13 @@ int main(){
     cout<< obj1.getname();
     cout<<obj1.getbalance();
     cout<<obj1.getaddress();
+    cout<<endl;
+    obj1.depositmoney();
+    cout<<endl;
+    obj1.withdrawwmoney();
+    cout<<endl;
+    cout<<obj1.getbalance();
+    cout<<"Here is your latest Account details"<<endl;
+    obj1.accountdetails();
     
 }
